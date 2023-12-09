c913c30e86813b23573831cd575420790849ddf4
Yogesh Pathe <yugyogeshpawar@gmail.com>
2023-12-09 18:24:15 +0530

    changes

--- src/sections/dashboard/contribute/create-contribute.js
+++ src/sections/dashboard/contribute/create-contribute.js
@@ -57,7 +57,6 @@ export const JobCreateForm = () => {
           <CardHeader
             subheader={
               <Typography
-                variant="h4"
                 sx={{
                   display: "flex",
                   justifyContent: "space-between",
