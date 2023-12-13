3e330cb95de7c2953a14fe33e2afe94494f26265
Abhi <abhippateltx@gmail.com>
2023-12-13 12:29:24 +0530

    investors-detail

--- src/pages/admin/investors/[investorsId]/index.js
+++ src/pages/admin/investors/[investorsId]/index.js
@@ -307,28 +307,8 @@ const Page = () => {
                 <Divider />
               </div>
             </Stack>
-            {currentTab === 'details' && (
-              <div>
-                <Grid
-                  container
-                  spacing={4}
-                >
-                  <Grid
-                    xs={12}
-                    lg={12}
-                  >
-                    <InvestorBasicDetails
-                    />
-                  </Grid>
-
-
-
-
-
-
-                </Grid>
-              </div>
-            )}
+            <Card>
+            {currentTab === 'details' &&  <InvestorBasicDetails/>} </Card>
             {currentTab === 'invoices' && <InvestorInvoices invoices={invoices} />}
 
           </Stack>
