7a702c791d3230174c1f4ff66721f863858ffe43
Abhi <abhippateltx@gmail.com>
2023-12-18 18:27:10 +0530

    deals-new

--- src/pages/dashboard/deals.js
+++ src/pages/dashboard/deals.js
@@ -1,7 +1,5 @@
-import { useCallback, useEffect, useState } from "react";
+
 import Head from "next/head";
-import NextLink from "next/link";
-import ArrowLeftIcon from "@untitled-ui/icons-react/build/esm/ArrowLeft";
 import {
   Avatar,
   Box,
@@ -25,24 +23,9 @@ import {
   Tooltip,
   Unstable_Grid2 as Grid,
 } from "@mui/material";
-import { jobsApi } from "../../api/deals";
-import { useMounted } from "../../hooks/use-mounted";
-import { usePageView } from "../../hooks/use-page-view";
 import { Layout as DashboardLayout } from "../../layouts/dashboard";
-import { paths } from "../../paths";
-import { CompanyActivity } from "../../sections/dashboard/deals/company-activity";
-import { CompanyAssets } from "../../sections/dashboard/deals/company-assets";
-import { ProjectInfo } from "../../sections/dashboard/deals/project-info";
-import { CompanySummary } from "../../sections/dashboard/deals/deals-info";
-import { getInitials } from "../../utils/get-initials";
-
-
-
-import { addDays, addHours, differenceInDays, isAfter } from 'date-fns';
 import DotsHorizontalIcon from '@untitled-ui/icons-react/build/esm/DotsHorizontal';
 import { Scrollbar } from '../../components/scrollbar';
-
-const now = new Date();
 
 const tasks = [
   {
@@ -53,29 +36,8 @@ const tasks = [
         name: 'Marcus Finn'
       }
     ],
-    title: 'Update the API for the project',
-    button: 'Sold Out'
-  },
-  {
-    id: '5eff24bb5bb3bd1beeddde78',
-    members: [
-      {
-        avatar: '/assets/avatars/avatar-penjani-inyene.png',
-        name: 'Penjani Inyene'
-      }
-    ],
-    title: 'Redesign the landing page',
-    button: 'Sold Out'
-  },
-  {
-    id: '5eff24c019175119993fc1ff',
-    members: [
-      {
-        avatar: '/assets/avatars/avatar-miron-vitold.png',
-        name: 'Miron Vitold'
-      }
-    ],
-    title: 'Solve the bug for the showState',
+    title: 'bitsCrunch Community Sale - Sold Out!',
+    desc: 'The final community sale of the year is here,and the waiting room is open!Sale starts December 14,2023 at 17:00 UTC and has one public option.',
     button: 'Sold Out'
   },
 ];
@@ -90,7 +52,8 @@ const tasks2 = [
         name: 'Marcus Finn'
       }
     ],
-    title: 'Release v1.0 Beta',
+    title: 'Chainflip Community Sale Has Ended',
+    desc: 'The efficient cross-chain swapping protocol. Not available in US,CN,CA, and other jurisdictions.Sale has ended.',
     button: 'Sold Out'
   },
   {
@@ -102,7 +65,8 @@ const tasks2 = [
         name: 'Jie Yan Song'
       }
     ],
-    title: 'GDPR Compliance',
+    title: 'The Archway Community Sale is Sold Out',
+    desc: 'The Value Capture Chain for the Cosmos.US,CA,CN,KR, and other jurisdictions excluded',
     button: 'Sold Out'
   },
   {
@@ -114,7 +78,21 @@ const tasks2 = [
         name: 'Penjani Inyene'
       }
     ],
-    title: 'Redesign Landing Page',
+    title: 'The Neon Community Sale is Sold Out',
+    desc: 'An Ethereum Virtual Machine on Solana. US,CA,CN,KR, and other jurisdictions excluded',
+    button: 'Sold Out'
+  },
+  {
+    id: '5eff24cf8740fc9faca4e463',
+    
+    members: [
+      {
+        avatar: '/assets/avatars/avatar-penjani-inyene.png',
+        name: 'Penjani Inyene'
+      }
+    ],
+    title: 'The CyberConnect Community Sale is Sold Out',
+    desc: 'Web3s Earliest and Biggest Decentralized Social Network.US,CA,CN,KR, and other jurisdictions excluded',
     button: 'Sold Out'
   }
 ]
@@ -149,7 +127,7 @@ const Page = () => {
         />
         <Divider />
         <Scrollbar>
-          <List sx={{ minWidth: 400 }}>
+          <List sx={{ minWidth: 500 }}>
             {tasks.map((task, index) => {
               const showDivider = index < tasks.length - 1;
 
@@ -158,24 +136,30 @@ const Page = () => {
                   divider={showDivider}
                   key={task.id}
                 >
-                  <AvatarGroup max={3} sx={{margin:2}}>
+                  <AvatarGroup sx={{margin:2}}>
                     {task.members.map((member) => (
                         <Avatar src={member.avatar} />
                     ))}
                   </AvatarGroup>
                   <ListItemText
                     primary={(
+                      <>
                       <Link
                         color="text.primary"
                         noWrap
                         sx={{ cursor: 'pointer' }}
-                        variant="subtitle2"
+                        variant="subtitle"
                       >
                         {task.title}
                       </Link>
+                      <Typography variant="subtitle2">
+                  {task.desc}
+                  </Typography>
+                  </>
                     )}
                   />
-                    <Button variant="contained" color="primary">
+                  
+                    <Button variant="contained" color="primary" sx={{textWrap:'nowrap'}}>
                       {task.button}
                     </Button>
                 </ListItem>
@@ -197,7 +181,7 @@ const Page = () => {
         />
         <Divider />
         <Scrollbar>
-          <List sx={{ minWidth: 400 }}>
+          <List sx={{ minWidth: 500 }}>
             {tasks2.map((task, index) => {
               const showDivider = index < tasks.length - 1;
 
@@ -212,18 +196,23 @@ const Page = () => {
                     ))}
                   </AvatarGroup>
                   <ListItemText
-                    primary={(
-                      <Link
-                        color="text.primary"
-                        noWrap
-                        sx={{ cursor: 'pointer' }}
-                        variant="subtitle2"
-                      >
-                        {task.title}
-                      </Link>
-                    )}
+                  primary={(
+                    <>
+                    <Link
+                      color="text.primary"
+                      noWrap
+                      sx={{ cursor: 'pointer' }}
+                      variant="subtitle"
+                    >
+                      {task.title}
+                    </Link>
+                    <Typography variant="subtitle2">
+                {task.desc}
+                </Typography>
+                </>
+                  )}
                   />
-                    <Button variant="contained" color="primary">
+                    <Button variant="contained" color="primary" sx={{textWrap:'nowrap'}}>
                       {task.button}
                     </Button>
                 </ListItem>
