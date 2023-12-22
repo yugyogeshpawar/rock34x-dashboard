73ca75285584d7f5da07fce41e8a38612392e595
Abhi <abhippateltx@gmail.com>
2023-12-22 11:13:56 +0530

    bestbuy

--- src/sections/dashboard/overview/overview-best-buy.js
+++ src/sections/dashboard/overview/overview-best-buy.js
@@ -27,45 +27,46 @@ export const OverviewBest = () => {
     {
       id: '1',
       name: 'Bitcoin',
-      description: 'Digital gold',
-      desc: 'Bitcoin is a decentralized digital currency, without a central bank or single administrator, that can be sent from user to user on the peer-to-peer bitcoin network without the need for intermediaries.',
+      desc: 'Bitcoin is a decentralized digital currency',
+      // desc: 'Bitcoin is a decentralized digital currency, without a central bank or single administrator, that can be sent from user to user on the peer-to-peer bitcoin network without the need for intermediaries.',
       category: 'Accessories',
-      image: '/assets/products/product-1.png',
+      image: '/assets/deal-icon/icon.png',
       sales: 13153
     },
     {
       id: '2',
       name: 'Ethereum',
-      description: 'Smart contracts platform',
-      desc: 'Ethereum is a decentralized, open-source blockchain with smart contract functionality. Ether (ETH), the native cryptocurrency, is created and used to compensate participants who perform computations and validate transactions.',
+      desc: 'Ethereum is a decentralized, open-source blockchain with smart contract functionality',
+      // desc: 'Ethereum is a decentralized, open-source blockchain with smart contract functionality. Ether (ETH), the native cryptocurrency, is created and used to compensate participants who perform computations and validate transactions.',
       category: 'Accessories',
-      image: '/assets/products/product-2.png',
+      image: '/assets/deal-icon/icon2.png',
       sales: 10300
     },
     {
       id: '3',
       name: 'Binance Coin',
-      description: 'Cryptocurrency used to pay for transaction fees on the Binance exchange',
-      desc: 'Binance Coin (BNB) is a cryptocurrency used to pay for transaction fees on the Binance exchange. It can also be used for various other purposes within the Binance ecosystem.',
+      desc: 'Cryptocurrency used to pay for transaction fees on the Binance exchange',
+      // desc: 'Binance Coin (BNB) is a cryptocurrency used to pay for transaction fees on the Binance exchange. It can also be used for various other purposes within the Binance ecosystem.',
       category: 'Accessories',
+      image: '/assets/deal-icon/icon3.png',
       sales: 5300
     },
     {
       id: '4',
       name: 'Cardano',
-      description: 'Blockchain platform for the development of decentralized applications',
-      desc: 'Cardano is a blockchain platform for the development of decentralized applications (DApps). It aims to provide a more secure and scalable infrastructure for the development of smart contracts and DApps.',
+      desc: 'Blockchain platform for the development of decentralized applications',
+      // desc: 'Cardano is a blockchain platform for the development of decentralized applications (DApps). It aims to provide a more secure and scalable infrastructure for the development of smart contracts and DApps.',
       category: 'Accessories',
-      image: '/assets/products/product-4.png',
+      image: '/assets/deal-icon/icon4.png',
       sales: 1203
     },
     {
       id: '5',
       name: 'Solana',
-      description: 'High-performance blockchain supporting decentralized applications and crypto projects',
-      desc: 'Solana is a high-performance blockchain supporting decentralized applications and crypto projects. It aims to provide fast and low-cost transactions, making it suitable for a wide range of applications.',
+      desc: 'High-performance blockchain supporting decentralized applications and crypto projects',
+      // desc: 'Solana is a high-performance blockchain supporting decentralized applications and crypto projects. It aims to provide fast and low-cost transactions, making it suitable for a wide range of applications.',
       category: 'Accessories',
-      image: '/assets/products/product-5.png',
+      image: '/assets/deal-icon/icon5.png',
       sales: 254
     }
   ]
--- src/sections/dashboard/overview/overview-news.js
+++ src/sections/dashboard/overview/overview-news.js
@@ -1,98 +1,274 @@
-// Import necessary modules
-import { formatDistanceToNowStrict, subHours } from 'date-fns';
-import { Avatar, Box, Card, CardHeader, Link, Stack, Typography } from '@mui/material';
+// // Import necessary modules
+// import { formatDistanceToNowStrict, subHours } from 'date-fns';
+// import { Avatar, Box, Card, CardHeader, Link, Stack, Typography } from '@mui/material';
 
-// Get the current date
-const now = new Date();
+// // Get the current date
+// const now = new Date();
 
-// Sample crypto news data
-const cryptoNews = [
-  {
-    id: '1',
-    author: {
-      avatar: '/assets/deal-icon/icon.png',
-      name: 'Jane Doe'
+// // Sample crypto news data
+// const cryptoNews = [
+//   {
+//     id: '1',
+//     author: {
+//       avatar: '/assets/deal-icon/icon.png',
+//       name: 'Jane Doe'
+//     },
+//     headline: 'Major Blockchain Conference Scheduled for Next Month',
+//     createdAt: subHours(now, 1).getTime()
+//   },
+//   {
+//     id: '2',
+//     author: {
+//       avatar: '/assets/deal-icon/icon2.png',
+//       name: 'John Smith'
+//     },
+//     headline: 'New Decentralized Finance (DeFi) Protocol Launches on Ethereum',
+//     createdAt: subHours(now, 3).getTime()
+//   },
+//   // {
+//   //   id: '3',
+//   //   author: {
+//   //     avatar: '/assets/deal-icon/icon3.png',
+//   //     name: 'Alice Wonder'
+//   //   },
+//   //   headline: 'Bitcoin Adoption Continues to Grow as Major Retailer Accepts BTC Payments',
+//   //   createdAt: subHours(now, 5).getTime()
+//   // },
+//   // Add more news items as needed
+// ];
+
+// // React component for displaying crypto news
+// export const OverviewNews = () => (
+//   <Card 
+//     sx={{
+//       p: 2
+//     }}
+//   >
+//   <Link color="text.primary" href='/dashboard/cryptonews'>
+//     <CardHeader sx={{px:2, py:2}}
+//       title="Crypto News"
+//     />
+//     </Link>
+//     <Stack spacing={1}>
+//       {cryptoNews.map((news) => {
+//         const ago = formatDistanceToNowStrict(news.createdAt);
+
+//         return (
+//           <Card key={news.id}>
+//             <CardHeader
+//               avatar={<Avatar src={news.author.avatar} />}
+//               disableTypography
+//               subheader={(
+//                 <Typography
+//                   color="text.secondary"
+//                   variant="body2"
+//                 >
+//                   {ago}
+//                   {' '}
+//                   ago
+//                 </Typography>
+//               )}
+//               title={(
+//                 <Link
+//                   color="text.primary"
+//                   variant="subtitle2"
+//                 >
+//                   {news.author.name}
+//                 </Link>
+//               )}
+//             />
+//             <Box
+//               sx={{
+//                 pb: 2,
+//                 px: 3
+//               }}
+//             >
+//               <Typography
+//                 color="text.secondary"
+//                 variant="body1"
+//               >
+//                 {news.headline}
+//               </Typography>
+//             </Box>
+//           </Card>
+//         );
+//       })}
+//     </Stack>
+//   </Card>
+// );
+
+
+// import PropTypes from 'prop-types';
+import React from 'react';
+import numeral from 'numeral';
+import ArrowRightIcon from '@untitled-ui/icons-react/build/esm/ArrowRight';
+import Image01Icon from '@untitled-ui/icons-react/build/esm/Image01';
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardHeader,
+  Stack,
+  SvgIcon,
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+  Typography
+} from '@mui/material';
+import { MoreMenu } from '../../../components/more-menu';
+import { Scrollbar } from '../../../components/scrollbar';
+
+export const OverviewNews = () => {
+  // const { products } = props;
+
+  const products = [
+    {
+      id: '1',
+      name: 'Bitcoin',
+      description: 'Digital gold',
+      desc: 'Bitcoin is a decentralized digital currency, without a central bank or single administrator, that can be sent from user to user on the peer-to-peer bitcoin network without the need for intermediaries.',
+      category: 'Accessories',
+      image: '/assets/covers/crypto-news-1.jpg',
+      sales: 13153
     },
-    headline: 'Major Blockchain Conference Scheduled for Next Month',
-    createdAt: subHours(now, 1).getTime()
-  },
-  {
-    id: '2',
-    author: {
-      avatar: '/assets/deal-icon/icon2.png',
-      name: 'John Smith'
+    {
+      id: '2',
+      name: 'Ethereum',
+      description: 'Smart contracts platform',
+      desc: 'Ethereum is a decentralized, open-source blockchain with smart contract functionality. Ether (ETH), the native cryptocurrency, is created and used to compensate participants who perform computations and validate transactions.',
+      category: 'Accessories',
+      image: '/assets/covers/crypto-news-2.png',
+      sales: 10300
     },
-    headline: 'New Decentralized Finance (DeFi) Protocol Launches on Ethereum',
-    createdAt: subHours(now, 3).getTime()
-  },
-  // {
-  //   id: '3',
-  //   author: {
-  //     avatar: '/assets/deal-icon/icon3.png',
-  //     name: 'Alice Wonder'
-  //   },
-  //   headline: 'Bitcoin Adoption Continues to Grow as Major Retailer Accepts BTC Payments',
-  //   createdAt: subHours(now, 5).getTime()
-  // },
-  // Add more news items as needed
-];
+    {
+      id: '3',
+      name: 'Binance Coin',
+      description: 'Cryptocurrency used to pay for transaction fees on the Binance exchange',
+      desc: 'Binance Coin (BNB) is a cryptocurrency used to pay for transaction fees on the Binance exchange. It can also be used for various other purposes within the Binance ecosystem.',
+      category: 'Accessories',
+      image: '/assets/covers/crypto-news-3.jpg',
+      sales: 5300
+    },
+    {
+      id: '4',
+      name: 'Cardano',
+      description: 'Blockchain platform for the development of decentralized applications',
+      desc: 'Cardano is a blockchain platform for the development of decentralized applications (DApps). It aims to provide a more secure and scalable infrastructure for the development of smart contracts and DApps.',
+      category: 'Accessories',
+      image: '/assets/covers/crypto-news-4.png',
+      sales: 1203
+    },
+    // {
+    //   id: '5',
+    //   name: 'Solana',
+    //   description: 'High-performance blockchain supporting decentralized applications and crypto projects',
+    //   desc: 'Solana is a high-performance blockchain supporting decentralized applications and crypto projects. It aims to provide fast and low-cost transactions, making it suitable for a wide range of applications.',
+    //   category: 'Accessories',
+    //   image: '/assets/products/product-5.png',
+    //   sales: 254
+    // }
+  ]
 
-// React component for displaying crypto news
-export const OverviewNews = () => (
-  <Card 
-    sx={{
-      p: 2
-    }}
-  >
-  <Link color="text.primary" href='/dashboard/cryptonews'>
-    <CardHeader sx={{px:2, py:2}}
-      title="Crypto News"
-    />
-    </Link>
-    <Stack spacing={1}>
-      {cryptoNews.map((news) => {
-        const ago = formatDistanceToNowStrict(news.createdAt);
+  return (
+    <Card>
+      <CardHeader
+        action={<MoreMenu />}
+        title="Best Buy"
+      />
+      <Scrollbar>
+        <Table sx={{ minWidth: 300 }}>
+          <TableBody>
+            {products.map((product, index) => {
+              // const sales = numeral(product.sales).format('0,0');
 
-        return (
-          <Card key={news.id}>
-            <CardHeader
-              avatar={<Avatar src={news.author.avatar} />}
-              disableTypography
-              subheader={(
-                <Typography
-                  color="text.secondary"
-                  variant="body2"
+              return (
+                <TableRow
+                  hover
+                  key={product.id}
                 >
-                  {ago}
-                  {' '}
-                  ago
-                </Typography>
-              )}
-              title={(
-                <Link
-                  color="text.primary"
-                  variant="subtitle2"
-                >
-                  {news.author.name}
-                </Link>
-              )}
-            />
-            <Box
-              sx={{
-                pb: 2,
-                px: 3
-              }}
-            >
-              <Typography
-                color="text.secondary"
-                variant="body1"
-              >
-                {news.headline}
-              </Typography>
-            </Box>
-          </Card>
-        );
-      })}
-    </Stack>
-  </Card>
-);
+                  <TableCell>
+                    <Stack
+                      alignItems="center"
+                      direction="row"
+                      spacing={2}
+                    >
+                      {product.image
+                        ? (
+                          <Box
+                            sx={{
+                              alignItems: 'center',
+                              backgroundColor: 'neutral.50',
+                              backgroundImage: `url(${product.image})`,
+                              backgroundPosition: 'center',
+                              backgroundSize: 'cover',
+                              borderRadius: 1,
+                              display: 'flex',
+                              height: 80,
+                              justifyContent: 'center',
+                              overflow: 'hidden',
+                              width: 80,
+                              minWidth:80
+                            }}
+                          />
+                        )
+                        : (
+                          <Box
+                            sx={{
+                              alignItems: 'center',
+                              backgroundColor: (theme) => theme.palette.mode === 'dark'
+                                ? 'neutral.700'
+                                : 'neutral.50',
+                              borderRadius: 1,
+                              display: 'flex',
+                              height: 80,
+                              justifyContent: 'center',
+                              width: 80,
+                              minWidth:80
+                            }}
+                          >
+                            <SvgIcon>
+                              <Image01Icon />
+                            </SvgIcon>
+                          </Box>
+                        )}
+                      <div>
+                        <Typography variant="subtitle2">
+                          {product.name}
+                        </Typography>
+                        <Typography
+                          color="text.secondary"
+                          variant="body2"
+                        >
+                          in {product.desc}
+                        </Typography>
+                      </div>
+                    </Stack>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Scrollbar>
+      <CardActions sx={{ justifyContent: 'flex-end' }}>
+        <Button
+          color="inherit"
+          endIcon={(
+            <SvgIcon>
+              <ArrowRightIcon />
+            </SvgIcon>
+          )}
+          size="small"
+        >
+          See All
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+// EcommerceProducts.propTypes = {
+//   products: PropTypes.array.isRequired
+// };
